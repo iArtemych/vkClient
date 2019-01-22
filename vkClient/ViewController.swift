ee0df5dfea54cfa07ad15ab40b941c2ee635c540
Artem Chursin <tema320d@icloud.com>
2019-01-22 14:04:24 +0300

    Initial Commit

---
+++ vkClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkClient
+//
+//  Created by Artem Chursin on 22/01/2019.
+//  Copyright © 2019 Artem Chursin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
